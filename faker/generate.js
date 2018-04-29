35627b44761fc0013be49605b5beabe1bf7dbd54
Anderson Tsang <andyderson@gmail.com>
2018-04-28 17:52:57 -0700

    Eslint Implemented

--- faker/generate.js
+++ faker/generate.js
@@ -5,14 +5,14 @@ faker.locale = 'en_US';
 
 const results = [];
 
-for (var i = 0; i < 250; i++) {
+for (let i = 0; i < 250; i++) {
   faker.seed(i);
   const product = {};
   product.id = i; // Generate ID
   product.rating = Math.random() * 5; // Generate Rating
   product.name = faker.commerce.productName(); // Generate Name
-//   product.price = faker.commerce.price(); // Generate Price
-//   product.category = faker.commerce.product(); // Generate Category
+  //   product.price = faker.commerce.price(); // Generate Price
+  //   product.category = faker.commerce.product(); // Generate Category
   product.imageUrl = faker.image.imageUrl(); // Generate Image
   results.push(product);
 }
